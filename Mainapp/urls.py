2e722cda7f5ba3d1e172e55cd0dcea9a07722215
AnJiye <oan215@naver.com>
2022-01-23 17:49:55 +0900

    로그인 페이지

--- Mainapp/urls.py
+++ Mainapp/urls.py
@@ -6,7 +6,6 @@ app_name = 'Mainapp'
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('home/', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
 ]
