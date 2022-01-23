3fbaf43366d9397fafb0bb20c363239495147671
정수빈 <vywl0119@naver.com>
2022-01-23 22:15:59 +0900

    메뉴바, 회원가입 css 맞추고 scrap페이지 생성

--- Boardapp/views.py
+++ Boardapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def board(request):
     return render(request, 'Board/board.html')
+
+def scrap(request):
+    return render(request, 'Board/scrap.html')
