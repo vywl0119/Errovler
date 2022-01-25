dcab0bd0e6f0122e01d015b56e31f2c5a6f85def
boooyeon <boooyeon@gmail.com>
2022-01-25 23:40:05 +0900

    버그수정: 글이 있어야면 보여주기, 더보기 링크 수정

--- Boardapp/views.py
+++ Boardapp/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 def board(request):
     qna_boards = QnA_Board.objects
     #모든 글들을 대상으로
-    qna_board_list=Board.objects.all().order_by('-b_date')
+    qna_board_list=QnA_Board.objects.all().order_by('-qna_date')
     #블로그 객체 세 개를 한 페이지로 자르기
     paginator = Paginator(qna_board_list,9)
     #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
