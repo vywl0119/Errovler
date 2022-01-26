0533665a4f115089f4059809397ee7fefa66a2b7
정수빈 <vywl0119@naver.com>
2022-01-26 22:08:22 +0900

    category url수정 및 태그 수정

--- Boardapp/urls.py
+++ Boardapp/urls.py
@@ -7,15 +7,9 @@ app_name = 'Boardapp'
 urlpatterns = [
     path('qna_board/', views.qna_board, name='qna_board'),
     path('sol_board/', views.sol_board, name='sol_board'),
- 
 
-    path('sol_python/', views.sol_python, name='sol_python'),
-    path('sol_django/', views.sol_django, name='sol_django'),
-    path('sol_etc/', views.sol_etc, name='sol_etc'),
-   
-    path('qna_python/', views.qna_python, name='qna_python'),
-    path('qna_django/', views.qna_django, name='qna_django'),
-    path('qna_etc/', views.qna_etc, name='qna_etc'),
+    path('qna_category/<str:category>', views.qna_category, name='qna_category'),
+    path('sol_category/<str:category>', views.sol_category, name='sol_category'),
     
     path('mypage/', views.mypage, name='mypage'),
     
--- Boardapp/views.py
+++ Boardapp/views.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.shortcuts import render
 from Mainapp.models import Board, QnA_Board
 from django.core.paginator import Paginator
@@ -14,6 +15,16 @@ def qna_board(request):
     page = request.GET.get('page')
     #request된 페이지를 얻어온 뒤 return 해 준다
     qna_posts = paginator.get_page(page)
+
+    # category_list = QnA_Board.objects.values('category')
+    # seen = []
+    # for key, val in category_list.items():
+    #     if val not in seen:
+    #         seen.append(val)
+            
+    # for i in seen:
+    #     print(i)
+
     return render(request, 'Board/qna_board.html', {'qna_boards' : qna_boards, 'qna_posts':qna_posts})
 
 def sol_board(request):
@@ -32,82 +43,6 @@ def mypage(request):
     return render(request, 'Board/mypage.html')
 
 
-
-
-def sol_python(request):
-    boards = Board.objects
-    #모든 글들을 대상으로
-    board_list=Board.objects.filter(category='python').order_by('-b_date')
-    #블로그 객체 세 개를 한 페이지로 자르기
-    paginator = Paginator(board_list,9)
-    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
-    page = request.GET.get('page')
-    #request된 페이지를 얻어온 뒤 return 해 준다
-    posts = paginator.get_page(page)
-    return render(request, 'Board/sol_board.html', {'boards' : boards, 'posts':posts})
-
-def sol_django(request):
-    boards = Board.objects
-    #모든 글들을 대상으로
-    board_list=Board.objects.filter(category='Django').order_by('-b_date')
-    #블로그 객체 세 개를 한 페이지로 자르기
-    paginator = Paginator(board_list,9)
-    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
-    page = request.GET.get('page')
-    #request된 페이지를 얻어온 뒤 return 해 준다
-    posts = paginator.get_page(page)
-    return render(request, 'Board/sol_board.html', {'boards' : boards, 'posts':posts})
-
-def sol_etc(request):
-    boards = Board.objects
-    #모든 글들을 대상으로
-    board_list=Board.objects.filter(category='etc').order_by('-b_date')
-    #블로그 객체 세 개를 한 페이지로 자르기
-    paginator = Paginator(board_list,9)
-    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
-    page = request.GET.get('page')
-    #request된 페이지를 얻어온 뒤 return 해 준다
-    posts = paginator.get_page(page)
-    return render(request, 'Board/sol_board.html', {'boards' : boards, 'posts':posts})
-
-
-
-def qna_python(request):
-    qna_boards = QnA_Board.objects
-    #모든 글들을 대상으로
-    qna_board_list=QnA_Board.objects.filter(category='python').order_by('-qna_date')
-    #블로그 객체 세 개를 한 페이지로 자르기
-    paginator = Paginator(qna_board_list,9)
-    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
-    page = request.GET.get('page')
-    #request된 페이지를 얻어온 뒤 return 해 준다
-    qna_posts = paginator.get_page(page)
-    return render(request, 'Board/qna_board.html', {'qna_boards' : qna_boards, 'qna_posts':qna_posts})
-
-def qna_django(request):
-    qna_boards = QnA_Board.objects
-    #모든 글들을 대상으로
-    qna_board_list=QnA_Board.objects.filter(category='Django').order_by('-qna_date')
-    #블로그 객체 세 개를 한 페이지로 자르기
-    paginator = Paginator(qna_board_list,9)
-    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
-    page = request.GET.get('page')
-    #request된 페이지를 얻어온 뒤 return 해 준다
-    qna_posts = paginator.get_page(page)
-    return render(request, 'Board/qna_board.html', {'qna_boards' : qna_boards, 'qna_posts':qna_posts})
-
-def qna_etc(request):
-    qna_boards = QnA_Board.objects
-    #모든 글들을 대상으로
-    qna_board_list=QnA_Board.objects.filter(category='etc').order_by('-qna_date')
-    #블로그 객체 세 개를 한 페이지로 자르기
-    paginator = Paginator(qna_board_list,9)
-    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
-    page = request.GET.get('page')
-    #request된 페이지를 얻어온 뒤 return 해 준다
-    qna_posts = paginator.get_page(page)
-    return render(request, 'Board/qna_board.html', {'qna_boards' : qna_boards, 'qna_posts':qna_posts})
-
 def search(request):
     qna_search_boards = QnA_Board.objects.all().order_by('-qna_date')
     q = request.POST.get('q', "") 
@@ -118,3 +53,27 @@ def search(request):
     
     else:
         return render(request, 'Board/search_board.html')
+
+def qna_category(request, category):
+    qna_boards = QnA_Board.objects
+    #모든 글들을 대상으로
+    qna_board_list=QnA_Board.objects.filter(category=category).order_by('-qna_date')
+    #블로그 객체 세 개를 한 페이지로 자르기
+    paginator = Paginator(qna_board_list,9)
+    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
+    page = request.GET.get('page')
+    #request된 페이지를 얻어온 뒤 return 해 준다
+    qna_posts = paginator.get_page(page)
+    return render(request, 'Board/qna_board.html', {'qna_boards' : qna_boards, 'qna_posts':qna_posts})
+
+def sol_category(request, category):
+    boards = Board.objects
+    #모든 글들을 대상으로
+    board_list=Board.objects.filter(category=category).order_by('-b_date')
+    #블로그 객체 세 개를 한 페이지로 자르기
+    paginator = Paginator(board_list,9)
+    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
+    page = request.GET.get('page')
+    #request된 페이지를 얻어온 뒤 return 해 준다
+    posts = paginator.get_page(page)
+    return render(request, 'Board/sol_board.html', {'boards' : boards, 'posts':posts})
