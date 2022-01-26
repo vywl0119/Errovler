2e2f3d80dca04157411231942b8b102d61bc6744
정수빈 <vywl0119@naver.com>
2022-01-26 10:22:06 +0900

    카테고리별 게시글 이동

--- Boardapp/urls.py
+++ Boardapp/urls.py
@@ -8,5 +8,13 @@ urlpatterns = [
     path('qna_board/', views.qna_board, name='qna_board'),
     path('sol_board/', views.sol_board, name='sol_board'),
     path('scrap/', views.scrap, name='scrap'),
+
+    path('sol_python/', views.sol_python, name='sol_python'),
+    path('sol_django/', views.sol_django, name='sol_django'),
+    path('sol_etc/', views.sol_etc, name='sol_etc'),
+   
+    path('qna_python/', views.qna_python, name='qna_python'),
+    path('qna_django/', views.qna_django, name='qna_django'),
+    path('qna_etc/', views.qna_etc, name='qna_etc'),
    
 ]
