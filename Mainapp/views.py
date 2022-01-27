d7c6dfb7a431aef00ca954abde668ef53cde2cd8
AnJiye <oan215@naver.com>
2022-01-27 19:45:54 +0900

    프로필 파일 업로드 구현

--- Mainapp/views.py
+++ Mainapp/views.py
@@ -37,7 +37,8 @@ def signup(request):
         form = UserForm(request.POST)
         print('d')
         if form.is_valid():
-            form.save()
+            u = form.save(commit=False)
+            
             # 이메일
             username = form.cleaned_data.get('username')
             # 비밀번호
@@ -47,13 +48,19 @@ def signup(request):
             # 프로필
             # profile_path = form.cleaned_data.get('last_name')
             # profile_path = request.FILES.get('last_name')
-            profile_path = request.POST.get('last_name')
+            profile_path = request.FILES.get('last_name')
+            name = profile_path.name
+            with open('media/%s' % name, 'wb') as file:
+                for chunk in profile_path.chunks():
+                    file.write(chunk)
             # profile_path = profile_image.value
             # upload_file = request.FILES.get('fileInput')
             # filepath = upload_file.name
             # profile_image = filepath
             # profile_image = upload(request)
 
+            u.last_name = name
+            u.save()
             user = authenticate(username=username, password=raw_password, first_name=first_name, last_name=profile_path)
             login(request, user)
             return render(request, 'Main/login.html')
