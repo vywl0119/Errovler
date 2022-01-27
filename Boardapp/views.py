3335948eb389ec66f6c3b1bdb9d7e4a2f7850769
AnJiye <oan215@naver.com>
2022-01-27 13:28:22 +0900

    checkpoint of errovler

--- Boardapp/views.py
+++ Boardapp/views.py
@@ -1,10 +1,6 @@
 from unicodedata import category
 from django.shortcuts import render
-<<<<<<< HEAD
 from Mainapp.models import Total_Board, Total_Comment, Total_Scrap
-=======
-from Mainapp.models import Board, QnA_Board, Scrap
->>>>>>> 6d47c4cf24804b4d3b7f5df9319621b25fab46ab
 from django.core.paginator import Paginator
 from django.db.models import Q
 
@@ -44,7 +40,6 @@ def sol_board(request):
     return render(request, 'Board/sol_board.html', {'sol_boards' : sol_boards, 'sol_posts':sol_posts})
 
 def mypage(request):
-<<<<<<< HEAD
         user = request.user.first_name
         user_scrap = Total_Scrap.objects.filter(writer=user)
         
@@ -85,28 +80,11 @@ def sol_category(request, category):
     #request된 페이지를 얻어온 뒤 return 해 준다
     sol_posts = paginator.get_page(page)
     return render(request, 'Board/sol_board.html', {'sol_boards' : sol_boards, 'sol_posts':sol_posts})
-=======
-    user = request.user.first_name
-    user_scrap = Scrap.objects.filter(writer=user)
-    
-    tag_list = ['Python', 'Django', 'etc']
-    scrap_cnt = []
-    
-    for tag in tag_list:
-        cnt = user_scrap.filter(category=tag).count()
-        scrap_cnt.append(cnt)
-        
-    context = { 'scrap_cnt' : scrap_cnt, 
-               }
-    
-    return render(request, 'Board/mypage.html', context)
->>>>>>> 6d47c4cf24804b4d3b7f5df9319621b25fab46ab
 
 def total_category(request, category):
     total_search_boards = Total_Board.objects.filter(category=category).order_by('-tb_date')
     q = request.POST.get('q', "") 
 
-<<<<<<< HEAD
     if q:
         print('q 들어옴')
         board_list = total_search_boards = total_search_boards.filter(
@@ -147,39 +125,3 @@ def total_search(request):
 
     else:
         return render(request, 'Mainapp:home')
-=======
-def search(request):
-    qna_search_boards = QnA_Board.objects.all().order_by('-qna_date')
-    q = request.POST.get('q', "") 
-
-    if q:
-        qna_search_boards = qna_search_boards.filter(title__icontains=q)
-        return render(request, 'Board/search_board.html', {'qna_search_boards' : qna_search_boards, 'q' : q})
-    
-    else:
-        return render(request, 'Board/search_board.html')
-
-def qna_category(request, category):
-    qna_boards = QnA_Board.objects
-    #모든 글들을 대상으로
-    qna_board_list=QnA_Board.objects.filter(category=category).order_by('-qna_date')
-    #블로그 객체 세 개를 한 페이지로 자르기
-    paginator = Paginator(qna_board_list,9)
-    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
-    page = request.GET.get('page')
-    #request된 페이지를 얻어온 뒤 return 해 준다
-    qna_posts = paginator.get_page(page)
-    return render(request, 'Board/qna_board.html', {'qna_boards' : qna_boards, 'qna_posts':qna_posts})
-
-def sol_category(request, category):
-    boards = Board.objects
-    #모든 글들을 대상으로
-    board_list=Board.objects.filter(category=category).order_by('-b_date')
-    #블로그 객체 세 개를 한 페이지로 자르기
-    paginator = Paginator(board_list,9)
-    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아냄 )
-    page = request.GET.get('page')
-    #request된 페이지를 얻어온 뒤 return 해 준다
-    posts = paginator.get_page(page)
-    return render(request, 'Board/sol_board.html', {'boards' : boards, 'posts':posts})
->>>>>>> 6d47c4cf24804b4d3b7f5df9319621b25fab46ab
--- Mainapp/static/js/main-slide.js
+++ Mainapp/static/js/main-slide.js
@@ -1,9 +1,41 @@
-date = new Date();
+// date = new Date();
 
-year = date.getFullYear();
-month = date.getMonth() + 1;
-today = date.getDate();
-tomorrow = today + 1;
+// year = date.getFullYear();
+// month = date.getMonth() + 1;
+// today = date.getDate();
+// tomorrow = today + 1;
 
-document.getElementById("current_date").innerHTML = year + "." + month + "." + today + " 오늘의 수업은?";
-document.getElementById("tomorrow_date").innerHTML = year + "." + month + "." + tomorrow + " 내일의 수업은?";
+// document.getElementById("current_date").innerHTML = year + "." + month + "." + today + " 오늘의 수업은?";
+// document.getElementById("tomorrow_date").innerHTML = year + "." + month + "." + tomorrow + " 내일의 수업은?";
+
+// var imgs = 3;
+// var now = 0;
+
+// function slide() {
+//     now = now == imgs ? 0: now += 1;
+
+//     $(".slidewrap .slidelist .slideitem").eq(now - 1).css({"margin-left": "-500px"});
+//     $(".slidewrap .slidelist .slideitem").eq(now).css({"margin-left": "0px"});
+// }
+
+// function start() {
+//     $(".slidewrap .slidelist .slideitem").eq(0).siblings().css({"margin-left": "-500px"});
+
+//     setInterval(function () { slide() }, 2000);
+// }
+
+// start()
+
+// var idx_lgth = $(".slidewrap .slidelist .slideitem").length;
+// var srt = 1;
+
+// setInterval(AutoRun, 3000);
+  
+// function AutoRun(){
+//   if(srt == idx_lgth){
+//       srt = 0;  
+//   }
+//   $("section>a").eq(srt).addClass('on').siblings().removeClass('on');
+//   $("#visual>div").eq(srt).addClass('on').siblings().removeClass('on');
+//   srt++;  
+// }
