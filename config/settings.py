80c3738006a8c1c0a2d5bf4aa4145fc3cc8b216a
boooyeon <boooyeon@gmail.com>
2022-01-28 04:55:17 +0900

    deploy heroku

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6^j47s(uri-a_85r)i@jc0zh^_7y+5$sh-up_&x_g(gj%%%f^&'
+
+# SECRET_KEY = '6^j47s(uri-a_85r)i@jc0zh^_7y+5$sh-up_&x_g(gj%%%f^&'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6^j47s(uri-a_85r)i@jc0zh^_7y+5$sh-up_&x_g(gj%%%f^&')
+
+출처: https://egg-money.tistory.com/115 [완숙의 블로그]
+# cast=bool 이 없으면 False 를 문자열로 인식하게됨.
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -57,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -158,3 +166,7 @@ SUMMERNOTE_CONFIG = {
     },
 }
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
